6de255c5c105e958a0f311eb0fb56553ed2dafe3
HardEnigma <155725617+HardEnigma@users.noreply.github.com>
2024-10-16 09:02:59 +0300

    prrr 10 Муравьёв и Рубцов

---
+++ Submission-of-Applications_Ilin/Submission-of-Applications_Ilin/Pages/Passport.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Submission_of_Applications_Ilin.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Passport.xaml
+    /// </summary>
+    public partial class Passport : Page
+    {
+        public Passport()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Parents stat = new Parents();
+            this.NavigationService.Navigate(stat);
+        }
+    }
+}
